2a8850c9cc82cb7840cfe0004a5471c87e9ce168
Dennis <dchavezromero@wpi.edu>
2021-02-10 17:21:01 -0500

    Function to convert quaternion values into euler angles

--- Code/b_term_scripts/AdafruitIMU.py
+++ Code/b_term_scripts/AdafruitIMU.py
@@ -1,6 +1,7 @@
 import time
-from adafruit_extended_bus import ExtendedI2C as I2C
+import math
 import adafruit_bno055
+from adafruit_extended_bus import ExtendedI2C as I2C
 
 
 class AdafruitIMU:
@@ -9,6 +10,7 @@ class AdafruitIMU:
         i2c = I2C(1)  # Device is /dev/i2c-1
         self.sensor = adafruit_bno055.BNO055_I2C(i2c)
         self.lastAngle = 0
+
         
     def angleWrap(self, angle):
         #if(angle != None):
@@ -33,3 +35,34 @@ class AdafruitIMU:
     def getGyro(self):
         gyro = self.sensor.gyro[0]
         return gyro
+
+    def euler_from_quaternion(self):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        x = self.sensor.quaternion[0]
+        y = self.sensor.quaternion[1]
+        z = self.sensor.quaternion[2]
+        w = self.sensor.quaternion[3]
+
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+
+        roll_x = math.degrees(roll_x)
+        pitch_y = math.degrees(pitch_y)
+        yaw_z = math.degrees(yaw_z)
+
+        return roll_x, pitch_y, yaw_z  # in degrees
