1d77125191e07309afbfdc5af25ec40f2fa544d0
Matt Langkamp <59946598+mklangkamp@users.noreply.github.com>
2021-10-07 00:36:57 -0400

    Changed ASCII 7 to 27, corrected weird tabs/spacings that were causing python errors

--- Code/small_bot/DriveController.py
+++ Code/small_bot/DriveController.py
@@ -72,15 +72,15 @@ while(running):
     charTyped = getChar()
 
     #When arrow keys are pressed, a series of three keypresses are registered.
-    #Each arrow keypress has ASCII characters of 7, then 91, followed by either
+    #Each arrow keypress has ASCII characters of 27, then 91, followed by either
     #A, B, C, or D. The following code is to interpret that and translate it into
     #The WASD controls already implemented.
-    #7,91,65: up, 7,91,66: down, 7,91,67: right, 7,91,68: left
+    #27,91,65: up, 27,91,66: down, 27,91,67: right, 27,91,68: left
 
     #If any of the characters that are in the series of messages are pressed,
     #Check if its the determining character (A,B,C,D) otherwise return to beginning
     #Of loop to receive remainder of data (including determining character)
-    if(charTyped != None and ([7,91,65,66,67,68].count(ord(charTyped)) != 0)):
+    if(charTyped != None and ([27,91,65,66,67,68].count(ord(charTyped)) != 0)):
         if(charTyped == "A"):
             charTyped = "w"
         elif(charTyped == "B"):
@@ -113,19 +113,19 @@ while(running):
         leftSpeed = leftSpeed + 5
         rightSpeed = rightSpeed - 5
     else:
-	    leftSpeed = 0
-	    rightSpeed = 0
+        leftSpeed = 0
+        rightSpeed = 0
 
     #Keep values in range of motor controllers.
     if(leftSpeed >= 100):
-	    leftSpeed = 100
+        leftSpeed = 100
     if(leftSpeed <= -100):
-	    leftSpeed = -100
+        leftSpeed = -100
 
     if(rightSpeed >= 100):
-	    rightSpeed = 100
+        rightSpeed = 100
     if(rightSpeed <= -100):
-	    rightSpeed = -100
+        rightSpeed = -100
     
     #If a character has been typed, update the PWM signals sent with that info. 
     if(charTyped != None):
